bf025470f8885397360ff0f14f575fa4b6324ee0
Valentyna Hryhorieva <meowww717@gmail.com>
2020-12-07 05:16:43 +0200

    Create app.js

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TableCaption from '../tableCaption';
+import TableBody from '../tablBbody';
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="container">
+            <TableCaption />
+            <TableBody />
+        </div>
+    );
+};
+
+export default App;
