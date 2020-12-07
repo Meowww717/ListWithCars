97cfc8965d9254ca4c572c98dda51236a16355e1
Valentyna Hryhorieva <meowww717@gmail.com>
2020-12-07 05:21:58 +0200

    Create tableCaption.js

---
+++ src/components/tableCaption/tableCaption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './tableCaption.css';
+
+const TableCaption = () => {
+    return (
+        <div className="alert alert-dismissible alert-success">Choose your favorite car!</div>
+    );
+};
+
+export default TableCaption;
